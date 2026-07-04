75840f3eab9df930999f42033747fea4a9086af4
Gaston <rierag877@gmail.com>
2026-07-04 16:41:12 -0300

    v0.2.0: editor de PDF con layout libre, proyectos nativos, renders planos, fixes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,8 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
